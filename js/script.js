eb77d31d1f86181a6cba2937c7389db6356b5467
Mateusz Paśko <mateusz.pasko.r@gmail.com>
2022-04-07 19:41:45 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -9,7 +9,17 @@
             done: true,
         },
     ];
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
 
+    }
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
     const render = () => {
         let htmlString = "";
 
@@ -17,35 +27,40 @@
             htmlString += `
             <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+            <button class="js-remove">usuń</button>
 
             ${task.content}
             </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-    const addNewTask = () => {
-        tasks.puch({
-            content: newTaskContent,
-        });
 
-        render();
-    }
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+        });
+    };
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        console.log(newTaskContent)
+
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
     const init = () => {
         render();
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            console.log(newTaskContent)
-
-            if (newTaskContent === "") {
-                return;
-            }
-
-        });
+        form.addEventListener("submit", onFormSubmit);
     };
     init()
 }
